3d6b482bc1453e84cd1c8e864fc5618d047e32cc
Bijiabo <bijiabo@gmail.com>
2016-02-08 20:54:22 +0800

    add sending view and resend button;

--- ChatBubbleUI/Controllers/ChatTableViewController.swift
+++ ChatBubbleUI/Controllers/ChatTableViewController.swift
@@ -24,7 +24,19 @@ class ChatTableViewController: UITableViewController {
         "They push the human race forward.",
         "And while some may see them as the crazy ones, we see genius.",
         "Because the people who are crazy enough to think they can change the world - are the ones who DO!",
-        "Here's To The Crazy Ones. The misfits. The rebels. The trouble-makers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules, and they have no respect for the status-quo. You can quote them, disagree with them, glorify, or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius.Because the people who are crazy enough to think they can change the world - are the ones who DO!"
+        "Here's To The Crazy Ones. The misfits. The rebels. The trouble-makers. The round pegs in the square holes. The ones who see things differently. They're not fond of rules, and they have no respect for the status-quo. You can quote them, disagree with them, glorify, or vilify them. About the only thing you can't do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, we see genius.Because the people who are crazy enough to think they can change the world - are the ones who DO!",
+        "Here's To The Crazy Ones.",
+        "The misfits.",
+        "The rebels.",
+        "The trouble-makers.",
+        "The round pegs in thesquare holes.",
+        "The ones who see things differently.",
+        "They're not fond of rules, and they have no respect for the status-quo.",
+        "You can quote them, disagree with them, glorify, or vilify them.",
+        "About the only thing you can't do is ignore them.",
+        "Because they change things.",
+        "They push the human race forward.",
+        "And while some may see them as the crazy ones, we see genius."
     ]
     
     override func viewDidLoad() {
@@ -49,10 +61,32 @@ class ChatTableViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("rightCell", forIndexPath: indexPath) as! RightPostTableViewCell
-        cell.content = data[indexPath.row]
-
-        return cell
+        
+        if indexPath.row % 2 == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("rightCell", forIndexPath: indexPath) as! RightPostTableViewCell
+            cell.content = data[indexPath.row]
+            
+            if indexPath.row + 2 == data.count {
+                cell.sending = true
+            } else {
+                cell.sending = false
+            }
+            
+            if indexPath.row + 4 == data.count {
+                cell.sendError = true
+            } else {
+                cell.sendError = false
+            }
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("leftCell", forIndexPath: indexPath) as! LeftPostTableViewCell
+            cell.content = data[indexPath.row]
+            
+            return cell
+        }
+        
+        
     }
     
 }
--- ChatBubbleUI/Views/LeftPostTableViewCell.swift
+++ ChatBubbleUI/Views/LeftPostTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  RightPostTableViewCell.swift
+//  LeftPostTableViewCell.swift
 //  ChatBubbleUI
 //
 //  Created by huchunbo on 16/2/8.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class RightPostTableViewCell: UITableViewCell {
+class LeftPostTableViewCell: UITableViewCell {
 
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var backgroundContainerView: UIView!
     @IBOutlet weak var tailImageView: UIImageView!
-    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
-    @IBOutlet weak var resendButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,21 +20,15 @@ class RightPostTableViewCell: UITableViewCell {
         
         selectionStyle = .None
         
-        backgroundContainerView.backgroundColor = UIColor(red:0, green:0.46, blue:1, alpha:1)
+        backgroundContainerView.backgroundColor = UIColor(red:0.9, green:0.91, blue:0.93, alpha:1)
         backgroundContainerView.layer.cornerRadius = 16.0
-        
-        activityIndicatorView.hidden = true
-        resendButton.hidden = true
-        resendButton.layer.cornerRadius = 10.0
-        resendButton.layer.borderColor = UIColor(red:0.97, green:0.2, blue:0.13, alpha:1).CGColor
-        resendButton.layer.borderWidth = 1.0
         
         _displayAnimation()
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
@@ -58,32 +50,8 @@ class RightPostTableViewCell: UITableViewCell {
         }
     }
     
-    var sending: Bool = false {
-        didSet {
-            
-            if sending {
-                activityIndicatorView.startAnimating()
-            } else {
-                activityIndicatorView.stopAnimating()
-            }
-            
-            activityIndicatorView.hidden = !sending
-        }
-    }
-    
-    var sendError: Bool = false {
-        didSet {
-            resendButton.hidden = !sendError
-        }
-    }
-    
-    @IBAction func tapResendButton(sender: AnyObject) {
-        
-    }
-    
-    
     private func _displayAnimation() {
-        frame.origin.x = frame.size.width
+        frame.origin.x = -frame.size.width
         
         UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.05, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { () -> Void in
             
